f58946b26bf3a1f989ee194f0368f0b530cf5245
Jasper Bok <hello@jasperbok.nl>
2014-03-14 00:17:47 +0100

    Added a title to the map and stored location data in another format.

--- assets/js/mapper.js
+++ assets/js/mapper.js
@@ -3,16 +3,16 @@ function calculateDimensions(locations) {
 
     _(locations).forEach(function(location) {
         if (!minX) {
-            minX = location[0];
-            maxX = location[0];
-            minY = location[1];
-            maxY = location[1];
+            minX = location.x;
+            maxX = location.x;
+            minY = location.y;
+            maxY = location.y;
         }
 
-        if (location[0] < minX) minX = location[0];
-        if (location[0] > maxX) maxX = location[0];
-        if (location[1] < minY) minY = location[1];
-        if (location[1] > maxY) maxY = location[1];
+        if (location.x < minX) minX = location.x;
+        if (location.x > maxX) maxX = location.x;
+        if (location.y < minY) minY = location.y;
+        if (location.y > maxY) maxY = location.y;
     });
 
     width = maxX - minX;
@@ -55,30 +55,31 @@ function showTooltip(e) {
 $(document).ready(function initPage() {
     // Default locations:
     var locations = [
-        [-320, 1131, 'Spawn'],
-        [540, 1156, 'Buildzone 1'],
-        [446, -647, 'Buildzone 2'],
-        [4001, 1384, 'Buildzone 3'],
-        [-69, 9908, 'Buildzone 4'],
-        [216, 14683, 'Buildzone 5'],
-        [5033, 9899, 'Buildzone 6'],
-        [7396, 10131, 'Buildzone 7'],
-        [4832, 14265, 'Buildzone 8'],
-        [-907, 13774, 'Buildzone 9'],
-        [-9858, -15051, 'Buildzone 10'],
-        [13089, -11226, 'Buildzone 11'],
-        [12576, -21484, 'Clayton'],
-        [2993, 13945, 'Prime Empire'],
-        [7530, 10881, 'Lustarvia'],
-        [-227, -711, 'Dawnfall'],
-        [4724, 8500, 'Coldwood'],
-        [3992, -808, 'Flappy Ville'],
-        [123, 563, 'Treasure Town'],
-        [3871, -812, 'Waffle Ville'],
-        [8538, 9367, 'Ninja Ville'],
-        [-10182, -15032, 'Betopia'],
-        [-470, 74, 'Baby Girl Town'],
-        [-152, 1033, '/warp forest']
+        {name: 'Spawn', type: 'special', x: -320, y: 1131},
+        {name: 'Buildzone 1', type: 'buildzone', x: 540, y: 1156},
+        {name: 'Buildzone 2', type: 'buildzone', x: 446, y: -647},
+        {name: 'Buildzone 3', type: 'buildzone', x: 4001, y: 1384},
+        {name: 'Buildzone 4', type: 'buildzone', x: -69, y: 9908},
+        {name: 'Buildzone 5', type: 'buildzone', x: 216, y: 14683},
+        {name: 'Buildzone 6', type: 'buildzone', x: 5033, y: 9899},
+        {name: 'Buildzone 7', type: 'buildzone', x: 7396, y: 10131},
+        {name: 'Buildzone 8', type: 'buildzone', x: 4832, y: 14265},
+        {name: 'Buildzone 9', type: 'buildzone', x: -907, y: 13774},
+        {name: 'Buildzone 10', type: 'buildzone', x: -9858, y: -15051},
+        {name: 'Buildzone 11', type: 'buildzone', x: 13089, y: -11226}
+        
+        // [12576, -21484, 'Clayton'],
+        // [2993, 13945, 'Prime Empire'],
+        // [7530, 10881, 'Lustarvia'],
+        // [-227, -711, 'Dawnfall'],
+        // [4724, 8500, 'Coldwood'],
+        // [3992, -808, 'Flappy Ville'],
+        // [123, 563, 'Treasure Town'],
+        // [3871, -812, 'Waffle Ville'],
+        // [8538, 9367, 'Ninja Ville'],
+        // [-10182, -15032, 'Betopia'],
+        // [-470, 74, 'Baby Girl Town'],
+        // [-152, 1033, '/warp forest']
     ];
 
     var dimensions = calculateDimensions(locations),
@@ -91,9 +92,9 @@ $(document).ready(function initPage() {
     $('#map').width($(window).width());
 
     _(locations).forEach(function(location) {
-        var posX = (location[0] - dimensions.minX) * scale,
-            posY = (location[1] - dimensions.minY) * scale,
-            label = location[2],
+        var posX = (location.x - dimensions.minX) * scale,
+            posY = (location.y - dimensions.minY) * scale,
+            label = location.name,
             el = '<div class="marker" style="top:'+posY+'px;left:'+posX+'px" data-label="'+label+'"></div>';
 
         $('#map').append(el);
